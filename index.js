6f5c7df4a3ca8da29165aaee136ec633ece6c1a1
Troy Lawrence <redirect@gmail.com>
2018-02-08 01:41:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,12 +9,7 @@ function theBeatlesPlay (musicians, instruments) {
 
 
 
-const facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-];
+
 
 
 
