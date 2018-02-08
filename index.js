abed01dd01713c84abec70f7279ad0d9dd44eaa1
Troy Lawrence <redirect@gmail.com>
2018-02-08 01:37:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,14 +13,24 @@ const facts = [
   "He hated the sound of his own voice"
 ];
 
+
+
+
+
+
 function johnLennonFacts (facts) {
   let i = 0;
   while (i <= facts.length) {
-    facts.i += "!!!";
+    facts[i] += "!!!";
     i++;
   }
   return facts;
 }
+
+
+
+
+
 
 function iLoveTheBeatles (n) {
   let love =[];
